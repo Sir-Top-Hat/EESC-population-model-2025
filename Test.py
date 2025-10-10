c5d20e0ecc74dd6faf250b2070fad006cf801f53
Sir Top Hat <Harrymberts@gmail.com>
2025-10-10 15:53:02 +1100

    Finish function

--- Test.py
+++ Test.py
@@ -1,3 +1,7 @@
 class organism:
-    def __init__(self):
-        pass
+    def __init__(self,name,birth_rate,death_rate,food=None):
+        self.name = name
+        self.birth_rate = birth_rate
+        self.death_rate = death_rate
+        self.food = food
+        
